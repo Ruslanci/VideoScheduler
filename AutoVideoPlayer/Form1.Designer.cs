fa0d1a8a1aa4e1c9b3fb01268e5bc000ad6a55aa
Ruslanci <eggaegon@gmail.com>
2024-05-22 22:22:36 +0500

    Добавьте файлы проекта.

---
+++ AutoVideoPlayer/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AutoVideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseVideoButton = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.addVideoButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chooseVideoButton
+            // 
+            this.chooseVideoButton.Location = new System.Drawing.Point(128, 46);
+            this.chooseVideoButton.Name = "chooseVideoButton";
+            this.chooseVideoButton.Size = new System.Drawing.Size(157, 30);
+            this.chooseVideoButton.TabIndex = 0;
+            this.chooseVideoButton.Text = "Выбрать видео";
+            this.chooseVideoButton.UseVisualStyleBackColor = true;
+            this.chooseVideoButton.Click += new System.EventHandler(this.chooseVideoButton_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(560, 56);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(557, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество повторений";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Время начала проигрывания видео";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(350, 56);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(87, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // addVideoButton
+            // 
+            this.addVideoButton.Location = new System.Drawing.Point(350, 102);
+            this.addVideoButton.Name = "addVideoButton";
+            this.addVideoButton.Size = new System.Drawing.Size(75, 23);
+            this.addVideoButton.TabIndex = 7;
+            this.addVideoButton.Text = "+";
+            this.addVideoButton.UseVisualStyleBackColor = true;
+            this.addVideoButton.Click += new System.EventHandler(this.addVideoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addVideoButton);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.chooseVideoButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chooseVideoButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button addVideoButton;
+    }
+}
+
---
+++ AutoVideoPlayer/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoVideoPlayer
+{
+    public partial class Form1 : Form
+    {
+
+        private string videoPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseVideoButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Video Files|*.mp4;*.avi;*.mkv";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                videoPath = ofd.FileName;
+            }
+        }
+
+        private void addVideoButton_Click(object sender, EventArgs e)
+        {
+            int verticalSpacing = 10; // Spacing between controls
+
+            // Create a new instance of the Button
+            Button newChooseVideoButton = new Button();
+            newChooseVideoButton.Location = new Point(128, this.chooseVideoButton.Bottom + verticalSpacing);
+            newChooseVideoButton.Name = "chooseVideoButton" + (this.Controls.OfType<Button>().Count(b => b.Name.StartsWith("chooseVideoButton")) + 1);
+            newChooseVideoButton.Size = new Size(157, 30);
+            newChooseVideoButton.Text = "Выбрать видео";
+            newChooseVideoButton.UseVisualStyleBackColor = true;
+            newChooseVideoButton.Click += new EventHandler(this.chooseVideoButton_Click);
+
+            // Create a new instance of the DateTimePicker
+            DateTimePicker newDateTimePicker = new DateTimePicker();
+            newDateTimePicker.Format = DateTimePickerFormat.Time;
+            newDateTimePicker.Location = new Point(350, this.dateTimePicker1.Bottom + verticalSpacing);
+            newDateTimePicker.Name = "dateTimePicker" + (this.Controls.OfType<DateTimePicker>().Count() + 1);
+            newDateTimePicker.Size = new Size(87, 20);
+
+            // Create a new instance of the NumericUpDown
+            NumericUpDown newNumericUpDown = new NumericUpDown();
+            newNumericUpDown.Location = new Point(560, this.numericUpDown1.Bottom + verticalSpacing);
+            newNumericUpDown.Name = "numericUpDown" + (this.Controls.OfType<NumericUpDown>().Count() + 1);
+            newNumericUpDown.Size = new Size(120, 20);
+            newNumericUpDown.Value = new decimal(new int[] { 1, 0, 0, 0 });
+
+            // Calculate new Y position for the addVideoButton
+            int newYPosition = Math.Max(newChooseVideoButton.Bottom, Math.Max(newDateTimePicker.Bottom, newNumericUpDown.Bottom)) + verticalSpacing;
+
+            // Update the position of addVideoButton
+            this.addVideoButton.Location = new Point(this.addVideoButton.Location.X, newYPosition);
+
+            // Add the new controls to the form
+            this.Controls.Add(newChooseVideoButton);
+            this.Controls.Add(newDateTimePicker);
+            this.Controls.Add(newNumericUpDown);
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

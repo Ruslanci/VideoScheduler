4b52f01b4e1f22069df508bef9da8a010874f38b
Ruslanci <eggaegon@gmail.com>
2024-05-31 17:04:41 +0500

    добавил VideoScheduler, разделил логику добавления видео и его проигрывания от интерфейса

--- AutoVideoPlayer/MainForm.cs
+++ AutoVideoPlayer/MainForm.cs
@@ -1,24 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using Newtonsoft.Json;
 using System.Windows.Forms;
-using WMPLib;
 
 namespace AutoVideoPlayer
 {
     public partial class MainForm : Form
     {
-
         private Timer checkTimeTimer;
-        private Queue<ScheduledVideo> scheduledVideos;
-        private ScheduledVideo currentVideo;
+        private VideoScheduler videoScheduler;
         private VideoLabelManager videoLabelManager;
         private string saveFilePath = "scheduledVideos.json";
-
 
         public MainForm()
         {
@@ -28,7 +19,9 @@ namespace AutoVideoPlayer
 
         public void InitializeMainForm()
         {
-            scheduledVideos = new Queue<ScheduledVideo>();
+            videoScheduler = new VideoScheduler(saveFilePath);
+            videoScheduler.OnPlayFile += PlayFile;
+            videoScheduler.OnMediaError += HandleMediaError;
 
             checkTimeTimer = new Timer();
             checkTimeTimer.Interval = 1000;
@@ -67,52 +60,10 @@ namespace AutoVideoPlayer
                     }
                 }
 
-
                 string videoPath = ofd.FileName;
-                TimeSpan videoDuration = GetVideoDuration(videoPath);
+                ScheduledVideo video = videoScheduler.AddVideo(videoPath, chosenTime, (int)numberOfPlays);
 
-                ScheduledVideo scheduledVideo = new ScheduledVideo
-                {
-                    VideoPath = videoPath,
-                    ScheduledTime = chosenTime,
-                    TimesToRepeat = (int)numberOfPlays,
-                    VideoDuration = videoDuration,
-                    IsPlaying = false
-                };
-                scheduledVideos.Enqueue(scheduledVideo);
-                SaveScheduledVideos();
-
-
-                Label videoLabel = new Label();
-                videoLabel.Text = scheduledVideo.ToString();
-                videoLabel.AutoSize = true;
-                videoLabel.Location = new Point(chooseVideoButton.Bottom, chooseVideoButton.Bottom + 15);
-
-
-                videoLabelManager.AddLabel(scheduledVideo.ToString());
-            }
-        }
-
-
-
-        private TimeSpan GetVideoDuration(string videoPath)
-        {
-            if (string.IsNullOrEmpty(videoPath))
-            {
-                throw new ArgumentException("Video path cannot be null or empty.", nameof(videoPath));
-            }
-
-            try
-            {
-                WindowsMediaPlayer wmp = new WindowsMediaPlayer();
-                IWMPMedia mediaInfo = wmp.newMedia(videoPath);
-                double durationInSeconds = mediaInfo.duration;
-
-                return TimeSpan.FromSeconds(durationInSeconds);
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException($"An error occurred while retrieving the video duration: {ex.Message}", ex);
+                videoLabelManager.AddLabel(video.ToString());
             }
         }
 
@@ -123,6 +74,12 @@ namespace AutoVideoPlayer
             this.TopMost = true;
         }
 
+        private void HandleMediaError(string message)
+        {
+            MessageBox.Show(message);
+            this.Close();
+        }
+
         private void Player_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
         {
             if (e.newState == (int)WMPLib.WMPPlayState.wmppsPlaying)
@@ -131,45 +88,18 @@ namespace AutoVideoPlayer
             }
             else if (e.newState == (int)WMPLib.WMPPlayState.wmppsMediaEnded)
             {
-                if (currentVideo != null && currentVideo.TimesToRepeat > 0)
-                {
-                    currentVideo.TimesToRepeat--;
-                    if (currentVideo.TimesToRepeat > 0)
-                    {
-                        axWindowsMediaPlayer1.Ctlcontrols.play();
-                    }
-                    else
-                    {
-                        currentVideo = null;
-                        scheduledVideos.Dequeue();
-                    }
-                }
+                videoScheduler.HandleMediaEnd();
             }
         }
 
         private void Player_MediaError(object sender, AxWMPLib._WMPOCXEvents_MediaErrorEvent e)
         {
-            MessageBox.Show("Ошибка при проигрывании медиафайла.");
-            this.Close();
+            videoScheduler.HandleMediaError();
         }
 
         private void CheckTimeTimer_Tick(object sender, EventArgs e)
         {
-            DateTime now = DateTime.Now;
-
-            foreach (var scheduledVideo in scheduledVideos)
-            {
-                if (scheduledVideo.TimesToRepeat <= 0)
-                {
-
-                }
-                if (scheduledVideo.ScheduledTime.TimeOfDay.Minutes <= now.TimeOfDay.Minutes && scheduledVideo.IsPlaying == false)
-                {
-                    PlayFile(scheduledVideo.VideoPath);
-                    scheduledVideo.IsPlaying = true;
-                    break;
-                }
-            }
+            videoScheduler.CheckAndPlayVideos();
         }
 
         private void Form1_Activated(object sender, EventArgs e)
@@ -180,31 +110,12 @@ namespace AutoVideoPlayer
             }
         }
 
-    
-
-        private void SaveScheduledVideos()
-        {
-            var scheduledVideosList = scheduledVideos.ToList();
-            string json = JsonConvert.SerializeObject(scheduledVideosList, Formatting.Indented);
-            File.WriteAllText(saveFilePath, json);
-        }
-
         private void LoadScheduledVideos()
         {
-            if (File.Exists(saveFilePath))
+            foreach (var video in videoScheduler.ScheduledVideos)
             {
-                string json = File.ReadAllText(saveFilePath);
-                var scheduledVideosList = JsonConvert.DeserializeObject<List<ScheduledVideo>>(json);
-
-                var validVideos = scheduledVideosList.Where(video => video.EndTime > DateTime.Now).ToList();
-                scheduledVideos = new Queue<ScheduledVideo>(validVideos);
-
-                foreach (var video in scheduledVideos)
-                {
-                    videoLabelManager.AddLabel(video.ToString());
-                }
+                videoLabelManager.AddLabel(video.ToString());
             }
         }
-
     }
 }
---
+++ AutoVideoPlayer/VideoScheduler.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using WMPLib;
+
+namespace AutoVideoPlayer
+{
+    public class VideoScheduler
+    {
+        public Queue<ScheduledVideo> ScheduledVideos { get; private set; }
+        private ScheduledVideo currentVideo;
+        private string saveFilePath;
+        private WindowsMediaPlayer wmp;
+
+        public event Action<string> OnPlayFile;
+        public event Action<string> OnMediaError;
+
+        public VideoScheduler(string saveFilePath)
+        {
+            this.saveFilePath = saveFilePath;
+            ScheduledVideos = new Queue<ScheduledVideo>();
+            wmp = new WindowsMediaPlayer();
+            LoadScheduledVideos();
+        }
+
+        public ScheduledVideo AddVideo(string videoPath, DateTime scheduledTime, int timesToRepeat)
+        {
+            TimeSpan videoDuration = GetVideoDuration(videoPath);
+
+            ScheduledVideo scheduledVideo = new ScheduledVideo
+            {
+                VideoPath = videoPath,
+                ScheduledTime = scheduledTime,
+                TimesToRepeat = timesToRepeat,
+                VideoDuration = videoDuration,
+                IsPlaying = false
+            };
+
+            ScheduledVideos.Enqueue(scheduledVideo);
+            SaveScheduledVideos();
+
+            return scheduledVideo;
+        }
+
+        public void CheckAndPlayVideos()
+        {
+            DateTime now = DateTime.Now;
+
+            foreach (var scheduledVideo in ScheduledVideos)
+            {
+                if (scheduledVideo.TimesToRepeat <= 0) continue;
+
+                if (scheduledVideo.ScheduledTime.TimeOfDay.Minutes <= now.TimeOfDay.Minutes && scheduledVideo.IsPlaying == false)
+                {
+                    PlayFile(scheduledVideo.VideoPath);
+                    scheduledVideo.IsPlaying = true;
+                    break;
+                }
+            }
+        }
+
+        private TimeSpan GetVideoDuration(string videoPath)
+        {
+            if (string.IsNullOrEmpty(videoPath))
+            {
+                throw new ArgumentException("Путь до видео не может быть пустым.", nameof(videoPath));
+            }
+
+            try
+            {
+                IWMPMedia mediaInfo = wmp.newMedia(videoPath);
+                double durationInSeconds = mediaInfo.duration;
+
+                return TimeSpan.FromSeconds(durationInSeconds);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Ошибка при получении длины видео: {ex.Message}", ex);
+            }
+        }
+
+        private void PlayFile(string url)
+        {
+            OnPlayFile?.Invoke(url);
+        }
+
+        public void HandleMediaEnd()
+        {
+            if (currentVideo != null && currentVideo.TimesToRepeat > 0)
+            {
+                currentVideo.TimesToRepeat--;
+                if (currentVideo.TimesToRepeat > 0)
+                {
+                    PlayFile(currentVideo.VideoPath);
+                }
+                else
+                {
+                    currentVideo = null;
+                    ScheduledVideos.Dequeue();
+                }
+            }
+        }
+
+        public void HandleMediaError()
+        {
+            OnMediaError?.Invoke("Ошибка при проигрывании медиафайла.");
+        }
+
+        private void SaveScheduledVideos()
+        {
+            var scheduledVideosList = ScheduledVideos.ToList();
+            string json = JsonConvert.SerializeObject(scheduledVideosList, Formatting.Indented);
+            File.WriteAllText(saveFilePath, json);
+        }
+
+        private void LoadScheduledVideos()
+        {
+            if (File.Exists(saveFilePath))
+            {
+                string json = File.ReadAllText(saveFilePath);
+                var scheduledVideosList = JsonConvert.DeserializeObject<List<ScheduledVideo>>(json);
+
+                var validVideos = scheduledVideosList.Where(video => video.EndTime > DateTime.Now).ToList();
+                ScheduledVideos = new Queue<ScheduledVideo>(validVideos);
+            }
+        }
+    }
+}
